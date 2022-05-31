da60a994a0bd1880ea6108f91349e3b359cfb4a6
MrSkinny <rgnet1@yahoo.com>
2022-05-31 13:52:27 -0700

    refactor numpad

--- src/Numpad.js
+++ src/Numpad.js
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import "./Numpad.css";
 
-export default function Numpad({ eps, onClose, typeEps, setEps }) {
+export default function Numpad({ x, onClose, typeX, setX, zeroOnOpen = false }) {
   useEffect(() => {
-    setEps(0);
-  }, [setEps]);
+    if (zeroOnOpen) setX(0);
+  }, [setX]);
 
   return (
     <>
@@ -14,48 +14,48 @@ export default function Numpad({ eps, onClose, typeEps, setEps }) {
       <div className="inner-numpad">
         <div className="numpad-display d-flex justify-content-center">
           <p>
-            {eps}
+            {x}
           </p> 
         </div>
         <div className="d-flex justify-content-between mx-5">
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(7)} className="numpad-btn btn btn-secondary">7</button>
+            <button onClick={() => typeX(7)} className="numpad-btn btn btn-secondary">7</button>
           </div>
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(8)} className="numpad-btn btn btn-secondary">8</button>
+            <button onClick={() => typeX(8)} className="numpad-btn btn btn-secondary">8</button>
           </div>
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(9)} className="numpad-btn btn btn-secondary">9</button>
+            <button onClick={() => typeX(9)} className="numpad-btn btn btn-secondary">9</button>
           </div>
         </div>
         <div className="d-flex justify-content-between mx-5">
         <div className="numpad-button-container">
-            <button onClick={() => typeEps(4)} className="numpad-btn btn btn-secondary">4</button>
+            <button onClick={() => typeX(4)} className="numpad-btn btn btn-secondary">4</button>
           </div>
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(5)} className="numpad-btn btn btn-secondary">5</button>
+            <button onClick={() => typeX(5)} className="numpad-btn btn btn-secondary">5</button>
           </div>
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(6)} className="numpad-btn btn btn-secondary">6</button>
+            <button onClick={() => typeX(6)} className="numpad-btn btn btn-secondary">6</button>
           </div>
         </div>
         <div className="d-flex justify-content-between mx-5">
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(1)} className="numpad-btn btn btn-secondary">1</button>
+            <button onClick={() => typeX(1)} className="numpad-btn btn btn-secondary">1</button>
           </div>
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(2)} className="numpad-btn btn btn-secondary">2</button>
+            <button onClick={() => typeX(2)} className="numpad-btn btn btn-secondary">2</button>
           </div>
           <div className="numpad-button-container">
-            <button onClick={() => typeEps(3)} className="numpad-btn btn btn-secondary">3</button>
+            <button onClick={() => typeX(3)} className="numpad-btn btn btn-secondary">3</button>
           </div>
         </div>
         <div className="d-flex justify-content-between mx-5">
           <div className="numpad-button-container-2">
-          <button onClick={() => typeEps(0)} className="numpad-btn btn btn-secondary">0</button>
+          <button onClick={() => typeX(0)} className="numpad-btn btn btn-secondary">0</button>
           </div>
           <div className="numpad-button-container">
-          <button onClick={() => setEps(0)} className="numpad-btn btn btn-secondary">C</button>
+          <button onClick={() => setX(0)} className="numpad-btn btn btn-secondary">C</button>
           </div>
         </div>
         <div className="d-flex justify-content-center my-3">
